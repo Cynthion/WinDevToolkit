45c211d75a5fd86bf9e34b21821b10a082e34c0e
christian.luethold <christian.luethold@hotmail.com>
2015-10-19 11:32:45 +0200

    tools added

---
+++ WPDevToolkit/SingletonLocator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using WPDevToolkit.Interfaces;
+
+namespace WPDevToolkit
+{
+    public static class SingletonLocator
+    {
+        private static readonly IDictionary<string, ILocatable> Dictionary = new ConcurrentDictionary<string, ILocatable>();
+
+        /*public static void Set<T>(T viewModel)
+        {
+            var type = typeof(T);
+            Dictionary.Add(type.FullName, viewModel);
+        }*/
+
+        public static T Get<T>() where T : ILocatable, new()
+        {
+            var type = typeof(T);
+            if (!Dictionary.ContainsKey(type.FullName))
+            {
+                var t = new T();
+                Dictionary.Add(t.GetType().FullName, t);
+            }
+            return (T)Dictionary[type.FullName];
+        }
+    }
+}
