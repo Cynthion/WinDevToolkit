8c763825db9431198f23d312a3859a6b31148f7a
christian.luethold <christian.luethold@hotmail.com>
2015-10-19 11:44:18 +0200

    2 classes added

---
+++ WPDevToolkit/UI/BaseTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WPDevToolkit.UI
+{
+    /// <summary>
+    /// Base class for all template selectors. Unwraps the necessary information such that subclasses
+    /// only need to deal with the type T by implementing <see cref="DoSelectTemplate"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseTemplateSelector<T> : DataTemplateSelector where T : class
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            var element = container as FrameworkElement;
+            if (element != null && item is T)
+            {
+                return DoSelectTemplate((T)item);
+            }
+            return null;
+        }
+
+        protected abstract DataTemplate DoSelectTemplate(T item);
+    }
+}
