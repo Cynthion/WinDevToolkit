1734bbf3b7c2d297c13b7a1ee366815ff2614494
christian.luethold <christian.luethold@hotmail.com>
2015-10-19 11:23:16 +0200

    classes added

---
+++ WPDevToolkit/BaseApi.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPDevToolkit
+{
+    public abstract class BaseApi
+    {
+        protected async Task<string> ApiGetRequestAsync(string url, string accessToken = null)
+        {
+            if (!PhoneInteraction.IsInternetAvailable())
+            {
+                await Messaging.ShowInternetUnavailableAsync();
+                return null;
+            }
+
+            ExceptionDispatchInfo exception = null;
+            try
+            {
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                if (request != null)
+                {
+                    request.Method = "GET";
+                    SetHeaders(request, accessToken);
+
+                    using (var response = await request.GetResponseAsync() as HttpWebResponse)
+                    {
+                        if (response != null)
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                return await reader.ReadToEndAsync();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                exception = ExceptionDispatchInfo.Capture(e);
+            }
+
+            if (exception != null)
+            {
+                // TODO make user-friendly
+                await Messaging.ShowMessageDialogErrorAsync(exception.SourceException);
+                //exception.Throw();
+            }
+            return null;
+        }
+
+        protected async Task<string> ApiPostRequestAsync(string url, string postJson, string accessToken = null)
+        {
+            if (!PhoneInteraction.IsInternetAvailable())
+            {
+                await Messaging.ShowInternetUnavailableAsync();
+                return null;
+            }
+
+            ExceptionDispatchInfo exception = null;
+            try
+            {
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                if (request != null)
+                {
+                    request.Method = "POST";
+                    request.ContentType = "application/json";
+                    SetHeaders(request, accessToken);
+
+                    // bytes
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postJson);
+
+                    using (var writer = await request.GetRequestStreamAsync())
+                    {
+                        writer.Write(byteArray, 0, byteArray.Length);
+                    }
+                    using (var response = await request.GetResponseAsync() as HttpWebResponse)
+                    {
+                        if (response != null)
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                return await reader.ReadToEndAsync();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                exception = ExceptionDispatchInfo.Capture(e);
+            }
+
+            if (exception != null)
+            {
+                await Messaging.ShowMessageDialogErrorAsync(exception.SourceException);
+            }
+            return null;
+        }
+
+        private void SetHeaders(HttpWebRequest request, string accessToken = null)
+        {
+            // set headers
+            request.Accept = "application/json";
+            DoSetHeaders(request);
+
+            //// set OAuth header
+            //if (accessToken != null)
+            //{
+            //    request.Headers[HttpRequestHeader.Authorization] = string.Format("Bearer {0}", accessToken);
+            //}
+        }
+
+        protected abstract void DoSetHeaders(HttpWebRequest request);
+    }
+}
---
+++ WPDevToolkit/IncrementalObservableCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Data;
+
+namespace WPDevToolkit
+{
+    // inspired by https://marcominerva.wordpress.com/2013/05/22/implementing-the-isupportincrementalloading-interface-in-a-window-store-app/
+
+    public interface IIncrementalSource<T>
+    {
+        Task<IEnumerable<T>> GetPagedItems(int pageIndex, int pageSize);
+
+        bool IsIncrementalLoading { get; }
+    }
+
+    public class IncrementalObservableCollection<TSource, T> : ObservableRangeCollection<T>, ISupportIncrementalLoading
+        where TSource : IIncrementalSource<T>, new()
+    {
+        private readonly TSource _source;
+        private readonly int _itemsPerPage;
+        private int _currentPage = 1; // start index 1
+
+        public IncrementalObservableCollection(TSource source, int itemsPerPage = 10)
+        {
+            _source = source;
+            _itemsPerPage = itemsPerPage;
+        }
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            HasMoreItems = true; // enable the first time
+            var dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
+
+            return Task.Run(async () =>
+            {
+                uint resCount = 0;
+                var result = await _source.GetPagedItems(_currentPage++, _itemsPerPage);
+                var resultArray = result as T[] ?? result.ToArray();
+                if (result == null || !resultArray.Any())
+                {
+                    HasMoreItems = false;
+                }
+                else
+                {
+                    resCount = (uint)resultArray.Length;
+
+                    // update UI
+                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        AddRange(resultArray);
+                    });
+                }
+                return new LoadMoreItemsResult { Count = resCount };
+            }).AsAsyncOperation();
+        }
+
+        public bool HasMoreItems { get; private set; }
+    }
+}
