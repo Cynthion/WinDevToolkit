b4beb12cdc7fbb2adafc120f2df1f2dadc1fcf74
christian.luethold <christian.luethold@hotmail.com>
2015-10-19 11:28:34 +0200

    Settings and SingletonLocator added

---
+++ WPDevToolkit/BaseSettings.cs
@@ -0,0 +1,45 @@
+﻿using Windows.Foundation.Collections;
+using Windows.Storage;
+
+namespace WPDevToolkit
+{
+    public abstract class BaseSettings
+    {
+        // settings
+        private const string IsFirstRunKey = "isfirstrun";
+
+        private static void Set<T>(string settingKey, T value)
+        {
+            var settings = GetLocalSettings();
+            if (settings.ContainsKey(settingKey) == false)
+            {
+                settings.Add(settingKey, value);
+            }
+            else
+            {
+                settings[settingKey] = value;
+            }
+        }
+
+        private static T Get<T>(string settingKey)
+        {
+            var settings = GetLocalSettings();
+            if (settings.ContainsKey(settingKey))
+            {
+                return (T)settings[settingKey];
+            }
+            return default(T);
+        }
+
+        private static IPropertySet GetLocalSettings()
+        {
+            return ApplicationData.Current.LocalSettings.Values;
+        }
+
+        public static bool IsFirstRun
+        {
+            get { return Get<bool>(IsFirstRunKey); }
+            set { Set(IsFirstRunKey, value); }
+        }
+    }
+}
---
+++ WPDevToolkit/Interfaces/ILocatable.cs
@@ -0,0 +1,8 @@
+﻿namespace WPDevToolkit.Interfaces
+{
+    /// <summary>
+    /// Marker interface for all locatable entities used by <see cref="SingletonLocator"/>.
+    /// </summary>
+    public interface ILocatable
+    { }
+}
