1a387a655c6ec25ed08e4472f1e77b80f7895fa3
christian.luethold <christian.luethold@hotmail.com>
2015-10-19 11:17:32 +0200

    classes added

---
+++ WPDevToolkit/PhoneInteraction.cs
@@ -0,0 +1,16 @@
+﻿using Windows.Networking.Connectivity;
+
+namespace WPDevToolkit
+{
+    public static class PhoneInteraction
+    {
+        private static readonly ConnectionProfile InternetProfile = NetworkInformation.GetInternetConnectionProfile();
+
+        public static bool IsInternetAvailable()
+        {
+            // TODO check correctness
+            return InternetProfile != null &&
+                   InternetProfile.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+        }
+    }
+}
