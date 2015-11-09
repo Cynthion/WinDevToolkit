795b95306b658342e4e9f849708190943ab7dbdb
Christian Lüthold <biocodr@gmail.com>
2015-11-09 22:27:12 +0100

    add missing files

---
+++ WPDevToolkit/ViewModel/AsyncLoaderBaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿//using System.Threading.Tasks;
+//using WPDevToolkit.Interfaces;
+
+//namespace WPDevToolkit.ViewModel
+//{
+//    public class AsyncLoaderBaseViewModel : BaseViewModel, IAsyncLoader
+//    {
+//        private readonly AsyncLoader _asyncLoader;
+
+//        public AsyncLoaderBaseViewModel(AsyncLoader asyncLoader)
+//        {
+//            _asyncLoader = asyncLoader;
+//        }
+
+//        // use AsyncLoader as a mixin class
+//        public Task LoadAsync()
+//        {
+//            return _asyncLoader.LoadAsync();
+//        }
+
+//        public bool IsLoading { get; private set; }
+//        public bool IsLoaded { get; private set; }
+//        public bool HasStatusText { get; private set; }
+//        public string StatusText { get; private set; }
+//    }
+//}
---
+++ WPDevToolkit/ViewModel/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WPDevToolkit.ViewModel
+{
+    /// <summary>
+    /// The abstract base class for all view models.
+    /// </summary>
+    public abstract class BaseViewModel : BaseNotifyPropertyChanged
+    {
+    }
+}
